4c5c39487aa4d8b7ed5a65160fed88bff5fd3dc5
muzam <muzamdty@gmail.com>
2023-05-14 14:34:55 +0100

    Connection build Successfuly

--- server/server.js
+++ server/server.js
@@ -1,6 +1,8 @@
 import express from "express";
 import mongoose from "mongoose";
 import cors from "cors";
+import bodyParser from "body-parser";
+import Transaction from "./models/Transaction.js";
 
 await mongoose.connect(
   "mongodb+srv://muzamdty:Zenbook1997@expensetracker.s7kx2gp.mongodb.net/?retryWrites=true&w=majority"
@@ -8,10 +10,22 @@ await mongoose.connect(
 console.log("MongoDB Connection is Successful");
 const PORT = 4000;
 const app = express();
-app.use(cors);
+app.use(cors());
+app.use(bodyParser.json());
 
 app.get("/", (req, res) => {
   res.send("Hello World");
+});
+
+app.post("/transaction", async (req, res) => {
+  const { amount, description, date } = req.body;
+  const transaction = new Transaction({
+    amount,
+    description,
+    date,
+  });
+  await transaction.save();
+  res.json({ message: "Success" });
 });
 
 app.listen(PORT, () => {
